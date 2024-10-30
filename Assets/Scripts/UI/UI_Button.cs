49d8c7d39ac5415b2fa4982384f76b39643a5e91
Alvina <alvinadr24@gmail.com>
2024-10-30 21:49:30 +0100

    feat: button animation

---
+++ Assets/Scripts/UI/UI_Button.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+using TMPro;
+
+public class UI_Button : MonoBehaviour, ISelectHandler, IDeselectHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private TextMeshProUGUI _textMeshProUGUI;
+    [SerializeField] private Image _background;
+
+    private void Awake()
+    {
+        GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        transform.DOScale(1f, .3f).SetUpdate(true);
+        _textMeshProUGUI.color = Color.black;
+        if (_background != null) _background.gameObject.SetActive(false);
+
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        transform.DOScale(1.1f, .3f).SetUpdate(true);
+        _textMeshProUGUI.color = Color.white;
+        if (_background != null) _background.gameObject.SetActive(true);
+    }
+
+    private void OnClick()
+    {
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        OnSelect(eventData);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        OnDeselect(eventData);
+    }
+}
