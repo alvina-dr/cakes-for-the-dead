47c85f86141db12b1358bce695a500591e7e409c
RenSunB0y <150889605+RenSunB0y@users.noreply.github.com>
2024-10-31 12:04:11 +0100

    push

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -21,13 +21,16 @@ public class AudioManager : MonoBehaviour
     #endregion
     public UnityEvent onGameStart = new UnityEvent();
     public UnityEvent onChaudronScene = new UnityEvent();
+    public UnityEvent onChaudronSceneOff = new UnityEvent();
     public UnityEvent onCut = new UnityEvent();
     public UnityEvent onCut2 = new UnityEvent();
     public UnityEvent onCut3 = new UnityEvent();
     public UnityEvent onPerfect = new UnityEvent();
     public UnityEvent onSoufflet = new UnityEvent();
     public UnityEvent onGrind = new UnityEvent();
+    public UnityEvent onGrind2 = new UnityEvent();
     public UnityEvent onPour = new UnityEvent();
+    public UnityEvent onPourOff = new UnityEvent();
     public UnityEvent onFireOn = new UnityEvent();
     public UnityEvent onFireOff = new UnityEvent();
 }
