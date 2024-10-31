2207ee54589308ef77365ea586f33585118321ef
Alvina <alvinadr24@gmail.com>
2024-10-31 12:25:07 +0100

    feat: bellows upgrade, now have more feedbacks and will stop if player got it wrong / accelerate if they got it right

--- Assets/Scripts/MiniGames/Bellows_MovingBar.cs
+++ Assets/Scripts/MiniGames/Bellows_MovingBar.cs
@@ -1,5 +1,6 @@
+using DG.Tweening;
 using System.Collections;
-using UnityEditor.Experimental.GraphView;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,14 +8,17 @@ public class Bellows_MovingBar : MonoBehaviour
 {
     public Bellows_Fire Fire;
     public UI_ShowSizeAnimation MovingCursor;
-    public float CursorSpeed;
     public Image TargetZone;
+    public UI_ShowSizeAnimation TargetZoneAnimation;
     public bool IsGoingRight = true;
     private float _leftMaxPosition;
     private float _rightMaxPosition;
     [SerializeField] private SpriteRenderer _bellowsSpriteRenderer;
     [SerializeField] private Sprite _bellowsOpen;
     [SerializeField] private Sprite _bellowsClose;
+    public float CurrentCursorSpeed;
+    [SerializeField] private List<float> _cursorSpeedList = new List<float>();
+
     private void Start()
     {
         RectTransform rectTransform = GetComponent<RectTransform>();
@@ -22,7 +26,6 @@ public class Bellows_MovingBar : MonoBehaviour
         _leftMaxPosition = rectTransform.position.x - rectTransform.sizeDelta.x/2;
         SetupTargetZone();
     }
-
 
     public void Update()
     {
@@ -38,7 +41,7 @@ public class Bellows_MovingBar : MonoBehaviour
             MovingCursor.transform.position = new Vector3(_leftMaxPosition, MovingCursor.transform.position.y, 0);
         }
 
-        MovingCursor.transform.position += new Vector3(CursorSpeed * (IsGoingRight ? 1 : -1) * Time.deltaTime, 0, 0);
+        MovingCursor.transform.position += new Vector3(CurrentCursorSpeed * (IsGoingRight ? 1 : -1) * Time.deltaTime, 0, 0);
     }
 
     public void CheckCursorPosition()
@@ -79,18 +82,25 @@ public class Bellows_MovingBar : MonoBehaviour
     public void ValidateCursorPosition()
     {
         Fire.AddFireStrength();
+        CurrentCursorSpeed = _cursorSpeedList[Fire.FireStrength];
         SetupTargetZone();
     }
 
     public void InvalidateCursorPosition()
     {
         Fire.ResetFireStrength();
+        CurrentCursorSpeed = 0;
+        DOVirtual.DelayedCall(.8f, () => CurrentCursorSpeed = _cursorSpeedList[Fire.FireStrength]);
     }
 
     public void SetupTargetZone()
     {
-        float totalWidth = _rightMaxPosition - _leftMaxPosition;
-        float randomPosition = Random.Range(0.0f, totalWidth) - totalWidth / 2 + transform.position.x;
-        TargetZone.transform.position = new Vector3(randomPosition, TargetZone.transform.position.y, 0);
+        TargetZoneAnimation.Hide(() =>
+        {
+            float totalWidth = _rightMaxPosition - _leftMaxPosition;
+            float randomPosition = Random.Range(0.0f, totalWidth) - totalWidth / 2 + transform.position.x;
+            TargetZone.transform.position = new Vector3(randomPosition, TargetZone.transform.position.y, 0);
+            TargetZoneAnimation.Show();
+        });
     }
 }
